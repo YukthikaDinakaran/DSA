6c859982720eae2ebcdb77e1403a324ca61ac0df
YukthikaDinakaran <167126996+YukthikaDinakaran@users.noreply.github.com>
2025-06-12 12:10:33 +0530

    Time: 3 ms (75.98%) | Memory: 44.4 MB (64.36%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,38 +10,59 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-
-        int interval = 1;
-        int n = lists.length;
-
-        while (interval < n) {
-            for (int i = 0; i + interval < n; i = i + interval * 2) {
-                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
-            }
-            interval *= 2;
+        if( lists == null || lists.length == 0){
+            return null;
         }
-
+        while(lists.length > 1){
+            List<ListNode> temp = new ArrayList<>();
+            for ( int i=0; i< lists.length; i= i+2){
+                ListNode l1 = lists[i];
+                ListNode l2;
+                if( i+1 < lists.length){
+                    l2 = lists[i+1];
+                }
+                else{
+                    l2 = null;
+                }
+                ListNode merging = mergelist( l1, l2);
+                temp.add(merging);
+            }
+            lists = new ListNode[temp.size()];
+            for (int i = 0; i < temp.size(); i++) {
+                lists[i] = temp.get(i);
+            }
+            
+        }
         return lists[0];
     }
-
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                current.next = l1;
-                l1 = l1.next;
-            } else {
-                current.next = l2;
-                l2 = l2.next;
-            }
-            current = current.next;
+    public ListNode mergelist(ListNode list1, ListNode list2) {
+        if(list1==null){
+            return list2;
         }
-
-        current.next = (l1 != null) ? l1 : l2;
-
-        return dummy.next;
-    }
+        if(list2==null){
+            return list1;
+        }
+        ListNode d = new ListNode();
+        ListNode c = d;
+        while(list1 !=null && list2 != null){
+            if(list1.val <= list2.val){
+                c.next = list1;
+                list1= list1.next;
+                
+            }
+            else{
+                c.next = list2;
+                list2 = list2.next;
+                
+            }
+            c = c.next;
+        }
+       if( list1!= null){
+        c.next = list1;
+       }
+       if( list2!= null){
+        c.next = list2;
+       }
+    return d.next;
+   }
 }
