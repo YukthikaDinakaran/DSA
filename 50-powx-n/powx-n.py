462ca19bd6ed2659b29eed5736c202a84e909382
Yukthika Dinakaran <167126996+YukthikaDinakaran@users.noreply.github.com>
2025-09-27 20:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (100.00%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        ans = x**n
+        return ans
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
