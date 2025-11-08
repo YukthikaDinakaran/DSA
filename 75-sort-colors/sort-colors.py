8812d561c7aade9b3ae14681368d804dababf1b4
Yukthika Dinakaran <167126996+YukthikaDinakaran@users.noreply.github.com>
2025-11-08 22:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.10%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,6 +1,19 @@
 class Solution(object):
     def sortColors(self, nums):
-        nums.sort()
+        n = len(nums)
+        c1 = 0
+        c2 = 0
+        c3 = 0
+        for i in range(n):
+            if nums[i] == 0:
+                c1 = c1+1
+            elif nums[i] == 1:
+                c2 = c2+1
+            else:
+                c3 = c3+1
+        nums[:] = [0]*c1+[1]*c2+[2]*c3
+        return nums
+
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
