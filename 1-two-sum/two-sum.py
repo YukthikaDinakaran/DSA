90492b49cf3262f22b3286f76ff4bf0db295cfc4
Yukthika Dinakaran <167126996+YukthikaDinakaran@users.noreply.github.com>
2025-12-30 15:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (30.50%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
         n = len(nums)
-        arr = [(num, i) for i, num in enumerate(nums)]
-        arr.sort()
-        left = 0
-        right = n -1
-        while(left < right):
-            if arr[left][0]+ arr[right][0] == target:
-                return [arr[left][1], arr[right][1]]
-            elif arr[left][0]+ arr[right][0] < target:
-                left = left+1
-            else:
-                right = right-1
+        mp = {}
+        for i in range(n):
+            j = target - nums[i]
+            if j in mp:
+                return [mp[j], i]
+            mp[nums[i]] = i 
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
