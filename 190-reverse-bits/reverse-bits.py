1326fe31141de74cfed6d64e6667d825749ee7c7
Yukthika Dinakaran <167126996+YukthikaDinakaran@users.noreply.github.com>
2026-01-02 06:06:48 +0530

    Time: 15 ms (74.41%) | Memory: 12.5 MB (8.17%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseBits(self, n):
+        res = 0
+        for _ in range(32):
+            bit = n&1
+            res = res <<1 | bit
+            n = n>>1
+        return res
+        """
+        :type n: int
+        :rtype: int
+        """
+        
